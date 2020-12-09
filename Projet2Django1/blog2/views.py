a556d31679c8bbb150097115e27dea376023d7f8
Idrissa Sacko <idrissasacko0@gmail.com>
2020-12-09 15:16:20 +0100

    affichage des données sur une page

--- Projet2Django1/blog2/views.py
+++ Projet2Django1/blog2/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-
+from .models import Apprenant
 
 def home(request,):
-    return render(request,"index.html")
+    return render(request,"index.html",{
+        "affichageA":Apprenant.objects.all()
+    
+    })
 
